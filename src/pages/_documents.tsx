d0b8aeaf747f193dbb2893b50e0fc8d525e65ad3
Kanta Kurzumi <krzmknt@gmail.com>
2023-07-09 21:14:37 +0900

    [Update] Setup tsconfig and styled-components

---
+++ src/pages/_documents.tsx
@@ -0,0 +1,34 @@
+import Document, { DocumentContext, DocumentInitialProps } from "next/document";
+import { ServerStyleSheet } from "styled-components";
+
+export class MyDocument extends Document {
+  static async getInitialProps(
+    ctx: DocumentContext
+  ): Promise<DocumentInitialProps> {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = (): any =>
+        originalRenderPage({
+          enhanceApp: (App: any) => (props: any) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+      const initialProps: DocumentInitialProps = await Document.getInitialProps(
+        ctx
+      );
+
+      return {
+        ...initialProps,
+        styles: (
+          <>
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </>
+        ),
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+}
