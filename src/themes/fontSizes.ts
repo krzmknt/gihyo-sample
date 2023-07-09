70765189161a6324edb0a158652c447ca299fcd7
Kanta Kurzumi <krzmknt@gmail.com>
2023-07-09 21:57:57 +0900

    [Update] setup storybook

---
+++ src/themes/fontSizes.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const fontSizes: any = [12, 14, 16, 20, 24, 32];
+
+// aliasess
+fontSizes.extraSmall = fontSizes[0];
+fontSizes.small = fontSizes[1];
+fontSizes.medium = fontSizes[2];
+fontSizes.mediumLarge = fontSizes[3];
+fontSizes.large = fontSizes[4];
+fontSizes.extraLarge = fontSizes[5];
---
+++ src/themes/space.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const space: any = ["0px", "8px", "16px", "32px", "64px"];
+
+// aliasess
+space.small = space[1];
+space.medium = space[2];
+space.large = space[3];
