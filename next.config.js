057e487abc895220fee6f8a017ead6aa61387939
Kanta Kurzumi <krzmknt@gmail.com>
2023-07-09 22:13:23 +0900

    [Update] Setup jest

--- next.config.js
+++ next.config.js
@@ -1,9 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  compiler: {
-    styledComponents: true,
-  },
+  compiler: (() => {
+    let compilerConfig = {
+      styledComponents: true,
+    };
+
+    if (process.env.NODE_ENV === "production") {
+      compilerConfig = {
+        ...compilerConfig,
+        // prd ではreact tresting library で使用する data-testid 属性を削除する
+        reactRemoveProperty: { properties: ["^data-testid$"] },
+      };
+    }
+    return compilerConfig;
+  })(),
 };
 
 module.exports = nextConfig;
